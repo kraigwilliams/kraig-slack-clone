c9ff7e3e09f0e7d06c8d2f111c9894f04bc4f41c
KRAIGYG <williamskrg@gmail.com>
2020-08-17 00:37:00 -0400

    start slack clone

---
+++ src/Header.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import {Avatar} from '@material-ui/icons/AccountCircle';
+
+
+export class Header extends Component {
+    render() {
+        return (
+            <div className="Header">
+              <div className='header_left'
+              
+              />  
+              <div className='header__searcb'
+              
+              /> 
+              <div className='header__right'
+              
+              />
+            </div>
+        )
+    }
+}
+
+export default Header
