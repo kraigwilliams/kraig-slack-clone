18fc3c2c511f1ba2f3150c80cb899755f586d496
KRAIGYG <williamskrg@gmail.com>
2020-08-22 14:35:47 -0400

    styling header

--- src/Header.js
+++ src/Header.js
@@ -9,12 +9,13 @@ export class Header extends Component {
   render() {
     return (
       <div className="header">
-          HI im the header
-        <div className="header_left">
+          
+        <div className="header__left">
           <Avatar className="header__avatar" />
           <AccessTimeIcon />
         </div>
-        <div className="header__searcb">
+        <div className="header__search">
+            <input type='search' placeholder='search kraig slack'/>
             <SearchIcon/>
         </div>
         <div className="header__right">
