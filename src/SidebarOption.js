5ce09ca7a8bf925483f763b4082f78f6c6da24e7
KRAIGYG <williamskrg@gmail.com>
2020-08-26 15:48:27 -0400

    continue sidebaroption component

--- src/SidebarOption.js
+++ src/SidebarOption.js
@@ -4,7 +4,11 @@ import './Sidebar.css'
 function SideBarOption({Icon, title}) {
     return (
         <div className='sidebarOption'>
-         {Icon && <Icon className='sidebarOption__icon'/>}   
+         {Icon && <Icon className='sidebarOption__icon'/>}
+         {Icon ? (<h3>{title}</h3>)
+          : (<h3>
+              <span className='sidebarOption__hash'>
+              #</span>{title}</h3>)}   
         </div>
     )
 }
